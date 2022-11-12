58f15880f9cd453d74603ad0ed4b91b43b6f4c41
Omar Shaalan <shaalan.omar007@gmail.com>
2022-11-12 19:50:14 +0200

    Update Ein.cpp

--- Ein.cpp
+++ Ein.cpp
@@ -5,12 +5,12 @@ using namespace std;
 
 int main()
 {
-    int index = 0, index2 =0;
+    int index = 0, instructionCount =0;
     cout<< "Enter below the index you start the memory with: ";
     cin >> index;
-    cout<< "Enter below the index you end the memory with: ";
-    cin>> index2;
-     for(index;index<index2*4;index++)
+    cout<< "Enter below the number of instructions you want to have";
+    cin>> instructionCount;
+     for(index;index<instructionCount*4;index++)
      {
         //if(index%4 == 0)
            
