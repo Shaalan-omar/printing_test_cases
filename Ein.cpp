5c58ea08dc11bf59c9ae87f4693148a851405f94
Omar Shaalan <shaalan.omar007@gmail.com>
2022-11-13 12:11:53 +0200

    Updates on file
    Ziad's Contribution

--- Ein.cpp
+++ Ein.cpp
@@ -20,3 +20,27 @@ int main()
      }
      return 0;
 }
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  long int index = 0, instructionCount = 0;
+  cout << "Enter below the index you start the memory with: ";
+  cin >> index;
+  cout << "Enter below the number of instructions you want to have";
+  cin >> instructionCount;
+  int count = index;
+  for (index; index < count + (instructionCount * 4); index++) {
+
+    cout << "{"
+         << "mem[" << index + 3 << "], "
+         << "mem[" << index + 2 << "], "
+         << "mem[" << index + 1
+         << "], mem["
+         << index << "]}"
+         << "=" << endl;
+    index = index + 3;
+  }
+  return 0;
+}
