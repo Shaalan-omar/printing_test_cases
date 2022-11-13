29a949df94a13be717bcdad52f8eab65ae4d2cb6
Omar Shaalan <shaalan.omar007@gmail.com>
2022-11-13 12:12:18 +0200

    Removing first edition

--- Ein.cpp
+++ Ein.cpp
@@ -1,29 +1,6 @@
 #include <iostream>
 using namespace std;
 
-
-
-int main()
-{
-    int index = 0, instructionCount =0;
-    cout<< "Enter below the index you start the memory with: ";
-    cin >> index;
-    cout<< "Enter below the number of instructions you want to have";
-    cin>> instructionCount;
-     for(index;index<instructionCount*4;index++)
-     {
-        //if(index%4 == 0)
-           
-
-            cout<<"{" << "mem["<< index+3<<"], "<< "mem["<< index+2<<"], "<<"mem["<< index+1<<"], ""mem["<< index<<"]}"<<"="<<endl;
-             index = index+3;
-     }
-     return 0;
-}
-
-#include <iostream>
-using namespace std;
-
 int main() {
   long int index = 0, instructionCount = 0;
   cout << "Enter below the index you start the memory with: ";
